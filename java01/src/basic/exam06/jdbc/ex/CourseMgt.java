0f2bd5778b2c86ad40ad635a05eb3f2447276eb5
bit <bit@bit-Ubuntu>
2014-04-08 14:35:18 +0900

    .

--- java01/src/basic/exam06/jdbc/ex/CourseMgt.java
+++ java01/src/basic/exam06/jdbc/ex/CourseMgt.java
@@ -1,23 +1,69 @@
 package basic.exam06.jdbc.ex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CourseMgt {
-	public static void insert() {
-		
+	static Connection con;
+	static Statement stmt;
+	
+	public static void insert(String title, String dest, int hours) throws SQLException {
+		stmt.executeUpdate("insert SE_COURS(TITLE, DEST, HOURS)" +
+				" value('" + title + "', '" + dest + "', '" + hours + "')");
+		System.out.println("----- 추가 됨");
 	}
 	
-	public static void list() {
-		
+	public static void list() throws SQLException {
+		ResultSet rs = stmt.executeQuery("select CNO, TITLE, DEST, HOURS from SE_COURS");
+		while(rs.next()) {
+			System.out.print(rs.getInt("CNO") + ",");
+			System.out.print(rs.getString("TITLE") + ",");
+			System.out.print(rs.getString("DEST") + ",");
+			System.out.println(rs.getString("HOURS"));
+		}
+		rs.close();
 	}
 	
-	public static void update() {
+	public static void update(int cno, String column, String value) throws SQLException {
 		
+		if (("TITLE").equals(column.toUpperCase())) {
+			stmt.executeUpdate("update SE_COURS set TITLE='" + value + "' where CNO=" + cno + "");
+			System.out.println("----- TITLE 변경 됨");
+		}	else if (("DEST").equals(column.toUpperCase())) {
+			stmt.executeUpdate("update SE_COURS set DEST='" + value + "' where CNO=" + cno + "");
+			System.out.println("----- DEST 변경 됨");
+		}	else if (("HOURS").equals(column.toUpperCase())) {
+			stmt.executeUpdate("update SE_COURS set HOURS='" + value + "' where CNO='" + cno + "'");
+			System.out.println("----- HOURS 변경 됨");
+		}	else {
+			System.out.println("----- 존재하지 않는 컬럼명입니다.");
+		}
 	}
 	
-	public static void delete() {
-		
+	public static void delete(int cno) throws SQLException {
+		stmt.executeUpdate("delete from SE_COURS where CNO=" + cno + "");
+		System.out.println("----- 삭제 됨");
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+	  Class.forName("com.mysql.jdbc.Driver");
+	  con = DriverManager.getConnection("jdbc:mysql://192.168.200.45:3306/studydb", "study", "study");
+	  stmt = con.createStatement();
 	  
+	  
+	  list();
+	  
+	  // insert("테스트", "홍길동", 00123);
+	  update(6, "hours", "00123");
+	  
+	  list();
+	  
+	  
+	  
+	  stmt.close();
+	  con.close();
   }
 }
