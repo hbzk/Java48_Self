6d4bbe8c3a174ee9dedfcef081ad1f2736f1b590
bit <bit@bit-Ubuntu>
2014-04-10 15:03:39 +0900

    .

---
+++ _0410_jdbc/src/jdbc/pm14/CourseDao.java
@@ -0,0 +1,13 @@
+package jdbc.pm14;
+
+public class CourseDao {
+	DBConnectionPool dbConnectionPool;
+	
+	public void setDBConnection(DBConnectionPool dbConnectionPool) {
+		this.dbConnectionPool = dbConnectionPool;
+	}
+	
+//	public insertDao() {
+//		
+//	}
+}
---
+++ _0410_jdbc/src/jdbc/pm14/CourseVo.java
@@ -0,0 +1,8 @@
+package jdbc.pm14;
+
+public class CourseVo {
+	int no;
+	String title;
+	String dest;
+	int hours;
+}
