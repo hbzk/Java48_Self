2c79b87cda54ff6b8525855aa30bb5397d5d50ff
bit <bit@bit-Ubuntu>
2014-05-01 15:21:29 +0900

    .

--- web02/src/main/java/mybatis/Test01.java
+++ web02/src/main/java/mybatis/Test01.java
@@ -18,6 +18,75 @@ public class Test01 {
 		
 		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
 		SqlSessionFactory factory = builder.build(configStream);
+		SqlSession sqlSession = factory.openSession(true);
+		
+		sqlSession.delete("test.subject.delete", 180);
+		
+		System.out.println("삭제 완료!");
+		sqlSession.close();
+	}
+	
+	public static void main04(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = Resources.getResourceAsStream(mybatisConfigFile); 
+		
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
+		
+		// autocommit
+		// - DML 실행 결과를 즉시 실제 DB에 반영하기
+		SqlSession sqlSession = factory.openSession(true);
+		
+		SubjectVo vo = new SubjectVo()
+			.setNo(77).setTitle("xxxxxxxxxxxxxㅌㅌㅌaaa").setDescription("xxxㅌㅌㅌaaa");
+		
+		sqlSession.update("test.subject.update", vo);
+		
+		System.out.println("변경 완료!");
+		sqlSession.close();
+	}
+	
+	public static void main03(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = Resources.getResourceAsStream(mybatisConfigFile); 
+		
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
+		SqlSession sqlSession = factory.openSession();
+		
+		// > .selectOne()의 파라미터는 객체만 가능해서, 77은 자동으로 오토박싱 처리 된다
+		SubjectVo vo = sqlSession.selectOne("test.subject.detail", 77); 
+		
+		System.out.format("%1$3d %2$s\n", vo.getNo(), vo.getTitle());
+		System.out.println(vo.getDescription());
+		
+		sqlSession.close();
+	}
+	
+	
+	public static void main02(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = Resources.getResourceAsStream(mybatisConfigFile); 
+		
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
+		SqlSession sqlSession = factory.openSession();
+		
+		SubjectVo vo = new SubjectVo()
+			.setTitle("aaaaa12").setDescription("asdasd");
+		
+		sqlSession.insert("test.subject.insert", vo);
+		sqlSession.commit();
+		sqlSession.close();
+	}
+	
+	
+	public static void main01(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = Resources.getResourceAsStream(mybatisConfigFile); 
+		
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
 		SqlSession sqlSession = factory.openSession();
 		
 		// SqlSession 객체를 통해 데이터베이스에 질의 한다.
