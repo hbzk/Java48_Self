a18a0ea3df8267c6936003011882a1d81f4628f7
bit <bit@bit-Ubuntu>
2014-05-02 16:49:03 +0900

    .

---
+++ web02/src/main/java/sems/services/StudentService.java
@@ -0,0 +1,14 @@
+package sems.services;
+
+import sems.vo.StudentVo;
+
+/* Service 객체의 호출 규칙 정의 = protocol = interface
+ * - 서비스 객체는 업무 절차를 정의하고 있다.
+ * - 트랜잭션을 정의 한다.
+ * - 메서드의 이름은 업무에서 사용하는 용어를 쓴다.
+ * 	예) 학생 추가 => insert(x) add(o) 
+ */
+public interface StudentService {
+	void add(StudentVo student);
+	
+}
---
+++ web02/src/main/java/sems/services/StudentServiceImpl.java
@@ -0,0 +1,12 @@
+package sems.services;
+
+import sems.vo.StudentVo;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public void add(StudentVo student) {
+		
+	}
+	
+}
