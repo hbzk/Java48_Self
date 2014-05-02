1777e9bb18a182bcef710b50144140a0e1d6a301
bit <bit@bit-Ubuntu>
2014-05-02 16:40:25 +0900

    .

--- web02/src/main/java/sems/controls/StudentControl.java
+++ web02/src/main/java/sems/controls/StudentControl.java
@@ -4,6 +4,10 @@ import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +32,11 @@ public class StudentControl {
 	
 	@Autowired
 	StudentDao studentDao;
+	
+	// 실제 아래의 인스턴스 변수에 주입되는 객체는
+	// beans.xml에서 설정한 DataSoureTransactionManager 이다.
+	@Autowired
+	PlatformTransactionManager txManager;
 	
 	// 기본 정보 입력 폼 출력
 	@RequestMapping(value="/insert", method=RequestMethod.GET)
@@ -60,9 +69,28 @@ public class StudentControl {
 	// 전체 입력 값 DB 저장
 	@RequestMapping(value="/insert4", method=RequestMethod.POST)
 	public String intsert4(@ModelAttribute("student") StudentVo student) {
-		userDao.insert(student);
-		studentDao.insert(student);
-		return "/student/insert4.jsp";
+		
+		// 1. 트랜잭션을 정의한다.
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		// 	1) 트랜잭션 이름
+		def.setName("tx1");
+		// 	2) 트랜잭션 관리 정책
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		
+		// 2. 트랜잭션 관리자로부터 트랜잭션 핸들러 얻기
+		TransactionStatus status = txManager.getTransaction(def);
+		try {
+			userDao.insert(student);
+			studentDao.insert(student);
+			txManager.commit(status);
+			return "/student/insert4.jsp";
+			
+		}	catch (Throwable ex) {
+			txManager.rollback(status);
+			// > RuntimeException => 메서드에 선언하지 않아도 호출자에게 보고 가능
+			throw new RuntimeException("학생 정보 입력 오류!", ex);
+			
+		}
 	}
 }
 
