39c10f92a550998d7016edc4d42b927391891af2
bit <bit@bit-Ubuntu>
2014-05-08 12:55:51 +0900

    .

--- web02/src/main/java/sems/controls/ajax/SubjectControl.java
+++ web02/src/main/java/sems/controls/ajax/SubjectControl.java
@@ -1,10 +1,6 @@
 package sems.controls.ajax;
 
-import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import sems.dao.SubjectDao;
+import sems.services.SubjectService;
 import sems.vo.AjaxResult;
 import sems.vo.SubjectVo;
 
@@ -22,99 +18,68 @@ import sems.vo.SubjectVo;
 @RequestMapping("/subject")
 public class SubjectControl {
 	static Logger log = Logger.getLogger(SubjectControl.class);
-	
+
 	@Autowired
-	SubjectDao subjectDao;
-	
+	SubjectService subjectService;
+
+	public SubjectControl() {
+		log.debug("SubjectControl 생성됨");
+	}
+
 	@RequestMapping("/list")
-  public AjaxResult list( 
-		@RequestParam(value="pageNo", defaultValue="1") int pageNo,
-		@RequestParam(value="pageSize", defaultValue="10") int pageSize,
-		HttpSession session,
-		HttpServletResponse response,
-		Model model) {
-		
-		try {
-			HashMap<String, Integer> params = new HashMap<String, Integer>();
-			params.put("startIndex", (pageNo - 1) * pageSize);
-			params.put("pageSize", pageSize);
-			
-	    List<SubjectVo> list = subjectDao.list(params);
-	    int listSize = list.size();
-	    session.setAttribute("list", list);
-	    
-	    
-	    AjaxResult result = new AjaxResult().setStatus("ok").setData(list).setPageSize(listSize);
-	    
-	    response.setContentType("text/html;charset=UTF-8");
-	    return result;
-	    
-    } catch (Throwable ex) {
-			return new AjaxResult()
-					.setStatus("error").setData(ex.getMessage());
-    }
-  }
-	
+	public AjaxResult list(
+			@RequestParam(value="pageNo",defaultValue="1") int pageNo, 
+			@RequestParam(value="pageSize",defaultValue="10") int pageSize) {
+		List<SubjectVo>list =  subjectService.list(pageNo, pageSize);
+		return new AjaxResult().setStatus("ok").setData(list);
+	}
+
 	@RequestMapping("/detail")
-  public String detail(int no, Model model) {
-    try {
-    	SubjectVo subject = subjectDao.detail(no);
-	    model.addAttribute("subject", subject);
-	    return "subject/detail";
-	    
-    } catch (Throwable ex) {
-	    throw new Error(ex);
-    }
-  }
-	
+	public String detail(int no, Model model) {
+		model.addAttribute("subject", subjectService.detail(no));
+		return "subject/detail";
+	}
+
 	@RequestMapping(value="/insert", method=RequestMethod.GET)
 	public String form() {
 		return "subject/form";
 	}
-	
+
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	public String insert(SubjectVo vo) {
-		try {
-			subjectDao.insert(vo);
-			return "subject/insert";
-			
-		}	catch (Throwable ex) {
-			throw new Error(ex);
-		}
+		subjectService.add(vo);
+		return "subject/insert";
 	}
-	
+
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String updateForm(int no, Model model) {
-		try {
-			SubjectVo subject = subjectDao.detail(no);
-			model.addAttribute("subject", subject);
-			return "subject/updateform";
-
-		}	catch (Throwable ex) {
-			throw new Error(ex);
-		}
+		model.addAttribute("subject", subjectService.detail(no));
+		return "subject/updateform";
 	}
-	
+
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public String update(SubjectVo vo, Model model) {
-		try {
-				subjectDao.update(vo);
-				return "redirect:detail.bit?no=" + vo.getNo();
-				
-		}	catch (Throwable ex) {
-			throw new Error(ex);
-		}
+		subjectService.change(vo);
+		return "redirect:detail.bit?no=" + vo.getNo();
 	}
-	
+
 	@RequestMapping(value="/delete", method=RequestMethod.GET)
-  public String delete(int no) {
-		try {
-			subjectDao.delete(no);
-			return "redirect:list.bit?pageNo=1&pageSize=10";
-			
-		}	catch (Throwable ex) {
-			throw new Error(ex);
-		}
-  }
-	
+	public AjaxResult delete(int no) {
+		subjectService.remove(no);
+		return new AjaxResult().setStatus("ok");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- web02/src/main/webapp/subject/main.js
+++ web02/src/main/webapp/subject/main.js
@@ -1,106 +1,65 @@
+var currPageNo = 1;
+var pageSize = 10;
+
 $(document).ready(function(){
+	$('#header').load('../header.html');
+	$('#footer').load('../footer.html');
 	
-	
-	
-	var coo = 'Account :  ';
-	coo += $.cookie().loginEmail;
-	console.log(coo);
-	
-	
-	$('#header').append(coo);
-	
-	// 페이지 로드
-	var page = 1;
-	var list = '';
-	
-	
-	// 1페이지 로드
-	load();
-	$('#pre').css("display", "none");
-
-	$('#pre').on('click', function() {
-		page --;
-		
-		clear();
-		load();
-
-		
-	})
-	
-	
-	$('#next').on('click', function() {
-		page ++;
-		console.log(page);
-		
-		clear();
-		load();
-		
-		$('#pre').css("display", "block");
-	})
-
-	
-function load() {
-	$.ajax('list.ajax', {
-		type: 'POST',
-		dataType: 'json', 	// 서버에서 보내는 데이터의 형식 지정
-		data: { 			// 서버쪽으로 보내는 데이터
-			pageNo: page		//,
-			//pageSize: $('#password').val(),
-		},
-		success: function(Object){
-			//console.log(Object);
-			var list = Object.ajaxResult.data;
-			
-			var pageSize = Object.ajaxResult.pageSize;
-			
-			// 페이지가 1일때 안보이게
-			if (page != 1) {
-				$('#pre').css("display", "");
-			}	else {
-				$('#pre').css("display", "none");
-			}
-			
-			// 출력 갯수가 10개일때만 다음 버튼 보이게
-			if (pageSize == 10) {
-				$('#next').css("display", "");
-			}	else {
-				$('#next').css("display", "none");
-			}
-			
-			
-			//console.log();
-			var listTable = $('#listTable');
-			var tr, td = null; 
-			for (var i = 0; i < list.length; i++) {
-				var subject = list[i];
-				//console.log(subject);
-				console.log(subject.no, subject.title);
-				
-				tr = document.createElement('tr');
-				tr.setAttribute('class', 'listTr');
-				td = document.createElement('td');
-				td.innerHTML = subject.no;
-				tr.appendChild(td);
-				
-				td = document.createElement('td');
-				td.innerHTML = subject.title;
-				tr.appendChild(td);
-				
-				listTable.append(tr);
-			}
-			
-		}, 
-		error:function(xhr, status, errorThrown){
-			alert('오류 잡으세요');
-			console.log(status);
-			console.log(errorThrown);
+	$('#prevPage').click(function(event){
+		if (currPageNo > 1) {
+			loadSubjectList(currPageNo - 1);
 		}
 	})
-}
-
-function clear() {
-	$('.listTr').remove();
-}
-
 	
+	$('#nextPage').click(function(){
+		loadSubjectList(currPageNo + 1);
+	})
+	
+	// >>>>>>>>>>>>>>>> live litener 등록 <<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
+	$(document).on('click', 'button.rowDelBtn', function(){
+		$.getJSON(bit.contextRoot + '/subject/delete.ajax?no=' + $(this).attr('data-no'),
+			function(jsonObj){
+				var result = jsonObj.ajaxResult;
+				//if (result.status == "ok") {
+					loadSubjectList(currPageNo);
+				//}
+		});
+	})
+	
+	loadSubjectList(1);
 });
+
+
+function loadSubjectList(pageNo) {
+	$.getJSON(
+		bit.contextRoot + '/subject/list.ajax?pageNo=' + pageNo + '&pageSize=' + pageSize,
+		function(jsonObj){
+			var result = jsonObj.ajaxResult;
+			
+			if (result.data.length > 0) {
+				var table = $('#subjectList');
+				$('.dataRow').remove();
+				
+				$.each(result.data, function(index, subject){
+					$('<tr>')
+					.addClass('dataRow')
+					.append('<td>' + subject.no)
+					.append($('<td>')
+						.append($('<a>')
+							.attr('href', contextRoot + '/subject/detail.bit?no=' + subject.no)
+							.text(subject.title)
+						)
+					)
+					.append($('<td>')
+						.append($('<button>삭제</button>')
+							.addClass('rowDelBtn')
+							.addClass('btn btn-danger btn-xs')
+							.attr('data-no', subject.no)
+					))
+					.appendTo(table);
+				});
+				currPageNo = pageNo;
+				$('#currPageNo').text(pageNo);
+			}
+	});
+}
